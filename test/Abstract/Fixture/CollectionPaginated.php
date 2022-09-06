1e7919e039ad0a54ed3ef92c414ec8239f2c2106
Lenny ROUANET <lennyrouanet@users.noreply.github.com>
2022-09-06 09:52:02 +0200

    Collection paginated

---
+++ test/Abstract/Fixture/CollectionPaginated.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Test\Abstract\Fixture;
+
+use Test\Abstract\Fixture\Value;
+
+class CollectionPaginated extends \Phant\DataStructure\Abstract\CollectionPaginated
+{
+	public function addValue(Value $item)
+	{
+		parent::addItem($item);
+	}
+	
+	public function removeValue(Value $item): self
+	{
+		return parent::removeItem($item);
+	}
+	
+	protected static function unserializeItem(mixed $item): Value
+	{
+		return Value::unserialize($item);
+	}
+}
