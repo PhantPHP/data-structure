ad9ec40f8f75c151a20fee92b5838d3fad3ebcbc
Lenny ROUANET <lennyrouanet@users.noreply.github.com>
2022-09-21 08:55:33 +0200

    Refacto

--- component/Time/DateTime.php
+++ component/Time/DateTime.php
@@ -5,28 +5,14 @@ namespace Phant\DataStructure\Time;
 
 use Phant\Error\NotCompliant;
 
-class DateTime extends \Phant\DataStructure\Abstract\Value\Varchar
+class DateTime extends \Phant\DataStructure\Time\Date
 {
-	protected int $time;
-
 	public function __construct(string $date, string $format = 'Y-m-d H:i:s')
 	{
 		if (strtolower($date) == 'now') {
 			$date = 'now';
 		}
 		
-		$time = strtotime($date);
-		if ($time === false) {
-			throw new NotCompliant('Date: ' . $date);
-		}
-		$this->time = $time;
-		$date = date($format, $this->time);
-		
-		parent::__construct($date);
-	}
-	
-	public function getTime(): int
-	{
-		return $this->time;
+		parent::__construct($date, $format);
 	}
 }
