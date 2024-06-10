7292e266217c8e7759d25bf23504711f63a9b479
Lenny ROUANET <lennyrouanet@users.noreply.github.com>
2024-06-10 17:32:09 +0200

    Fix Company Fr CodeActivite

--- component/Company/Fr/CodeActivite.php
+++ component/Company/Fr/CodeActivite.php
@@ -13,6 +13,10 @@ class CodeActivite extends \Phant\DataStructure\Abstract\Value\Varchar
         $code = strtoupper($code);
         $code = trim($code);
 
+        if (preg_match('/^(\d{4})(\w{1})$/', $code)) {
+            $code = substr($code, 0, 2) . '.' . substr($code, 2, 5);
+        }
+
         parent::__construct($code);
     }
 }
